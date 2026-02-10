caa4381d46a26ed808dcb61d17370f729635f1c7
Maggots <gonda.akosdonat@gmail.com>
2026-02-10 10:18:33 +0100

    Refactored a number of tables in the database
    - updated/added stored procedures

--- API/database/torma.sql
+++ API/database/torma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2026 at 01:14 PM
+-- Generation Time: Feb 10, 2026 at 10:13 AM
 -- Server version: 12.1.2-MariaDB
 -- PHP Version: 8.2.12
 
@@ -25,7 +25,7 @@ DELIMITER $$
 --
 -- Procedures
 --
-CREATE DEFINER=`gonda`@`localhost` PROCEDURE `auth_user` (IN `pUsername` VARCHAR(50), IN `pPassword` VARCHAR(100))   BEGIN
+CREATE DEFINER=`gonda`@`localhost` PROCEDURE `authUser` (IN `pUsername` VARCHAR(50), IN `pPassword` VARCHAR(100))   BEGIN
 SELECT u.id, u.username, u.email
 FROM user u
 JOIN user_secret c ON u.username = c.username
@@ -38,24 +38,50 @@ INSERT INTO reservations (about,reservation_date,duration,user_id)
 VALUES(pAbout,pReservationDate,pDuration,pUserId);
 END$$
 
+CREATE DEFINER=`gonda`@`localhost` PROCEDURE `createUser` (IN `pUsername` VARCHAR(32), IN `pPassword` VARCHAR(32), IN `pEmail` VARCHAR(100), IN `pFirstname` VARCHAR(50), IN `pLastname` VARCHAR(50))   BEGIN
+INSERT INTO user(username, email, first_name, last_name)
+VALUES(pUsername, pEmail, pFirstname, pLastname);
+INSERT INTO user_secret(password, username)
+VALUES(SHA2(pPassword,256),pUsername);
+END$$
+
 CREATE DEFINER=`gonda`@`localhost` PROCEDURE `deleteReservation` (IN `pId` INT(12))   BEGIN
 DELETE FROM reservations WHERE id=pId;
 END$$
 
-CREATE DEFINER=`gonda`@`localhost` PROCEDURE `delete_user` (IN `pUsername` VARCHAR(50))   BEGIN
+CREATE DEFINER=`gonda`@`localhost` PROCEDURE `deleteUser` (IN `pUsername` VARCHAR(50))   BEGIN
 DELETE FROM user WHERE username = pUsername;
 END$$
 
+CREATE DEFINER=`gonda`@`localhost` PROCEDURE `getAllUsers` ()   BEGIN
+SELECT u.id AS "ID", u.username AS "USERNAME", CONCAT(u.last_name, " ", u.first_name) AS "NAME", u.role AS "ROLE", u.created_at AS "CREATED AT"
+FROM user u
+ORDER BY u.created_at DESC;
+END$$
+
+CREATE DEFINER=`gonda`@`localhost` PROCEDURE `getUserById` (IN `pId` INT(12))   BEGIN
+SELECT u.id AS "ID", u.username AS "USERNAME", u.email AS "E-MAIL", CONCAT(u.last_name, " ", u.first_name) AS "NAME", u.role AS "ROLE", u.created_at AS "CREATED AT"
+FROM user u
+WHERE u.id=pId;
+END$$
+
+CREATE DEFINER=`gonda`@`localhost` PROCEDURE `getUserByUsername` (IN `pUsername` VARCHAR(32))   BEGIN
+SELECT u.id, u.username, u.email, CONCAT(u.last_name, " ", u.first_name) AS "NAME", u.role, u.created_at 
+FROM user u 
+WHERE u.username = pUsername;
+END$$
+
 CREATE DEFINER=`gonda`@`localhost` PROCEDURE `getUserReservations` (IN `pUserId` INT(12))   BEGIN
-SELECT id, about, reservation_date, duration, reservation_submitted
+SELECT id AS "ID", about AS "INFO", reservation_date AS "RESERVATION DATE", duration AS "DURATION", reservation_submitted AS "SUBMITTED AT"
 FROM reservations
 WHERE user_id=pUserId
 ORDER BY reservation_date ASC;
 END$$
 
-CREATE DEFINER=`gonda`@`localhost` PROCEDURE `register_user` (IN `pUsername` VARCHAR(50), IN `pFirstName` VARCHAR(50), IN `pLastName` VARCHAR(50), IN `pEmail` VARCHAR(100), IN `pPassword` VARCHAR(100))   BEGIN
-INSERT INTO user(username, first_name, last_name, email) VALUES(pUsername, pFirstName, pLastName, pEmail);
-INSERT INTO user_secret(password) VALUES(SHA2(pPassword,256));
+CREATE DEFINER=`gonda`@`localhost` PROCEDURE `updatePassword` (IN `pUsername` VARCHAR(50), IN `pNewPass` VARCHAR(100))   BEGIN
+UPDATE user_secret
+SET password = SHA2(pNewPass, 256)
+WHERE username = pUsername;
 END$$
 
 CREATE DEFINER=`gonda`@`localhost` PROCEDURE `updateReservation` (IN `pId` INT(12), IN `pAbout` CHAR(255), IN `pReservationDate` DATETIME, IN `pDuration` TIME)   BEGIN
@@ -66,35 +92,18 @@ duration=pDuration
 WHERE id=pId;
 END$$
 
-CREATE DEFINER=`gonda`@`localhost` PROCEDURE `update_password` (IN `pUsername` VARCHAR(50), IN `pNewPass` VARCHAR(100))   BEGIN
-UPDATE user_secret
-SET password = SHA2(pNewPass, 256)
-WHERE username = pUsername;
-END$$
-
-CREATE DEFINER=`gonda`@`localhost` PROCEDURE `update_username` (IN `pUsername` VARCHAR(50), IN `pId` INT(12))   BEGIN
+CREATE DEFINER=`gonda`@`localhost` PROCEDURE `updateUsername` (IN `pUsername` VARCHAR(50), IN `pId` INT(12))   BEGIN
 UPDATE user
 SET username=pUsername
 WHERE id=pId;
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateUserRole` (IN `pId` INT(12), IN `pRole` VARCHAR(12))   BEGIN
+UPDATE user SET role=pRole
+WHERE id=pId;
+END$$
+
 DELIMITER ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id` int(12) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `messages_id` int(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -105,8 +114,7 @@ CREATE TABLE `admin` (
 CREATE TABLE `messages` (
   `id` int(12) NOT NULL,
   `content` varchar(150) NOT NULL,
-  `user_id` int(12) NOT NULL,
-  `admin_id` int(12) NOT NULL
+  `user_id` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- --------------------------------------------------------
@@ -118,8 +126,20 @@ CREATE TABLE `messages` (
 CREATE TABLE `orders` (
   `id` int(12) NOT NULL,
   `order_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `user_id` int(12) NOT NULL,
-  `product_id` int(12) NOT NULL
+  `user_id` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(12) NOT NULL,
+  `orders_id` int(12) NOT NULL,
+  `product_id` int(12) NOT NULL,
+  `quantity` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- --------------------------------------------------------
@@ -131,8 +151,9 @@ CREATE TABLE `orders` (
 CREATE TABLE `product` (
   `id` int(12) NOT NULL,
   `product` text NOT NULL,
-  `is_bundled` tinyint(1) NOT NULL DEFAULT 0,
-  `orders_id` int(12) NOT NULL
+  `quantity` smallint(5) UNSIGNED NOT NULL,
+  `in_stock` tinyint(1) NOT NULL DEFAULT 1,
+  `is_bundled` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- --------------------------------------------------------
@@ -158,25 +179,24 @@ CREATE TABLE `reservations` (
 
 CREATE TABLE `user` (
   `id` int(12) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `username` varchar(32) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `orders_id` int(12) DEFAULT NULL,
-  `messages_id` int(12) DEFAULT NULL,
-  `reservations_id` int(12) DEFAULT NULL
+  `role` varchar(12) NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `first_name`, `last_name`, `email`, `created_at`, `orders_id`, `messages_id`, `reservations_id`) VALUES
-(1, 'mintapeti123', 'Péter', 'Minta', 'mintapeter@citromail.hu', '2026-01-06 11:02:48', NULL, NULL, NULL),
-(2, 'jackgypsum', 'Jakab', 'Gipsz', 'gipszj@freemail.hu', '2026-01-06 11:06:03', NULL, NULL, NULL),
-(3, 'testuser', 'Test', 'User', 'test@example.com', '2026-01-26 12:47:12', NULL, NULL, NULL),
-(4, 'testuser2', 'Test', 'User', 'test2@example.com', '2026-01-27 10:16:47', NULL, NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `first_name`, `last_name`, `email`, `created_at`, `role`) VALUES
+(1, 'mintapeti123', 'Péter', 'Minta', 'mintapeter@citromail.hu', '2026-01-06 11:02:48', 'user'),
+(2, 'jackgypsum', 'Jakab', 'Gipsz', 'gipszj@freemail.hu', '2026-01-06 11:06:03', 'user'),
+(3, 'testuser', 'Test', 'User', 'test@example.com', '2026-01-26 12:47:12', 'user'),
+(4, 'testuser2', 'Test', 'User', 'test2@example.com', '2026-01-27 10:16:47', 'user'),
+(5, 'john_doe', 'John', 'Doe', 'john@example.com', '2026-02-09 12:27:01', 'admin');
 
 -- --------------------------------------------------------
 
@@ -186,7 +206,7 @@ INSERT INTO `user` (`id`, `username`, `first_name`, `last_name`, `email`, `creat
 
 CREATE TABLE `user_secret` (
   `id` int(12) NOT NULL,
-  `password` char(100) NOT NULL,
+  `password` text NOT NULL,
   `address` char(255) DEFAULT NULL,
   `username` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
@@ -199,43 +219,40 @@ INSERT INTO `user_secret` (`id`, `password`, `address`, `username`) VALUES
 (1, '123445678', '7630 Pécs, Diósi út 42.', NULL),
 (2, '123445678', '7630 Pécs, Diósi út 42.', NULL),
 (3, 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f', NULL, 'testuser'),
-(4, 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f', NULL, 'testuser2');
+(4, 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f', NULL, 'testuser2'),
+(5, 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f', NULL, 'john_doe');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username_2` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `fk_messages_id` (`messages_id`);
-
---
 -- Indexes for table `messages`
 --
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_user_id` (`user_id`),
-  ADD KEY `fk_admin_id` (`admin_id`);
+  ADD KEY `fk_user_id` (`user_id`);
 
 --
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_user_id` (`user_id`),
-  ADD KEY `fk_product_id` (`product_id`);
+  ADD KEY `fk_user_id` (`user_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `orders_id` (`orders_id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_orders_id` (`orders_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `reservations`
@@ -251,10 +268,7 @@ ALTER TABLE `reservations`
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `fk_orders_id` (`orders_id`),
-  ADD KEY `fk_reservations_id` (`reservations_id`),
-  ADD KEY `fk_messages_id` (`messages_id`);
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `user_secret`
@@ -268,12 +282,6 @@ ALTER TABLE `user_secret`
 --
 
 --
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
@@ -283,6 +291,12 @@ ALTER TABLE `messages`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
   MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
 
 --
@@ -301,43 +315,36 @@ ALTER TABLE `reservations`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user_secret`
 --
 ALTER TABLE `user_secret`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `admin`
---
-ALTER TABLE `admin`
-  ADD CONSTRAINT `fk_messages_id` FOREIGN KEY (`messages_id`) REFERENCES `messages` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
---
 -- Constraints for table `messages`
 --
 ALTER TABLE `messages`
-  ADD CONSTRAINT `fk_admin_id` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
--- Constraints for table `product`
+-- Constraints for table `order_items`
 --
-ALTER TABLE `product`
-  ADD CONSTRAINT `fk_orders_id` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `1` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 
 --
 -- Constraints for table `reservations`
@@ -346,16 +353,12 @@ ALTER TABLE `reservations`
   ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
--- Constraints for table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `fk_messages_id` FOREIGN KEY (`messages_id`) REFERENCES `messages` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_orders_id` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_reservations_id` FOREIGN KEY (`reservations_id`) REFERENCES `reservations` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
---
 -- Constraints for table `user_secret`
 --
 ALTER TABLE `user_secret`
   ADD CONSTRAINT `fk_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE NO ACTION;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
