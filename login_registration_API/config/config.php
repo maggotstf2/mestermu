68c025a945c7075ab228871fa09fb151f23dc212
tormae <tormaenci@gmail.com>
2026-03-05 16:32:59 +0100

    Create config.php

---
+++ login_registration_API/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST','localhost');
+define('DB_NAME','torma');
+define('DB_USER','root');
+define('DB_PASS','');
+
+//jwt konfig
+define('JWT_SECRET','29nWgfOq0vRplCHd0D0y8Bq4LbKRpzA7C2JRHKduBC0');
